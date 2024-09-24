f169a041d0c90b639a267b49f97b2e3d85030072
ndpdung25 <phuongdungnguyendat2002@gmail.com>
2024-09-24 13:43:38 +0700

    Implement error handlers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+
 const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
+const {
+   resourceNotFound,
+   handleError,
+} = require('./controllers/errors.controller');
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
    extended: true
 }));
+
 app.get('/', (req, res) => {
    return res.json(JSend.success());
 });
+
 contactsRouter.setup(app);
+
+app.use(resourceNotFound);
+app.use(handleError);
+
+
 module.exports = app;
